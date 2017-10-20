7bbe7e2ba9d7de114b14f1b26beed0434a53d28e
david2099 <loevenbruck.david@gmail.com>
2017-10-20 11:00:57 +0200

    chore: update to REAME file

--- App.js
+++ App.js
@@ -153,7 +153,6 @@ export default class App extends React.Component {
     componentDidMount() {
         this.loadDecks()
         setLocalNotification()
-        //clearLocalNotification()
     }
 
     render() {
